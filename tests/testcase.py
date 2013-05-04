a3ba8be8714621acb9dd18158dd7523f049109c1
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2013-05-05 01:00:18 +0200

    Improve unit tests

--- tests/testcase.py
+++ tests/testcase.py
@@ -7,44 +7,10 @@ import signal
 import unittest
 
 
-class MasterTestCase(unittest.TestCase):
+class CommonMSTestCase(unittest.TestCase):
 
     def setUp(self):
-        if os.path.exists("/tmp/mbsocket"):
-            os.unlink("/tmp/mbsocket")
-
-        self.uts = Popen(["./unit-test-server", "rtu"], stdout=PIPE, stderr=PIPE)
-        self.rec = Popen(["../src/mbrecorder"], stdout=PIPE)
-        self.col = Popen(["../src/mbcollect", "-f", "master-test-case.ini"], stdout=PIPE)
-        self.is_master = True
-
-    def tearDown(self):
-        self.col.send_signal(signal.SIGTERM)
-        self.rec.send_signal(signal.SIGTERM)
-        self.uts.send_signal(signal.SIGTERM)
-        self.col.wait()
-        self.rec.wait()
-        self.uts.wait()
-
-    def test_output(self):
-        # MBCOLLECT
-        # First line contains host name
-        line = self.col.stdout.readline()
-        if self.is_master:
-            m = re.search("Slave name (\w+), ID (\d+)", line)
-            self.assertEqual(m.group(2), "1")
-        else:
-            m = re.search("Server name (\w+), IP ([\.\d]+):(\d+)", line)
-            self.assertEqual(m.group(2), "127.0.0.1")
-            self.assertEqual(m.group(3), "1502")
-
-        server_name = m.group(1)
-        self.assertEqual(server_name, "hello")
-
-        # Check parsing of values to read
-        c = re.compile("Address (\d+) => (\d+) values \((\w+)\)")
-
-        expected_values = [
+        self.expected_values = [
             {'a': 0, 'l': 2, 'type': 'floatmsb', 'values': ['12345.599609']},
             {'a': 2, 'l': 2, 'type': 'floatlsb', 'values': ['789.000000']},
             {'a': 0, 'l': 4, 'type': 'floatmsb', 'values': [
@@ -52,24 +18,24 @@ class MasterTestCase(unittest.TestCase):
             {'a': 4, 'l': 1, 'type': 'int', 'values': ['1']},
             {'a': 4, 'l': 3, 'type': 'int', 'values': ['1', '2', '3']},
         ]
-        for d in expected_values:
-            m = c.match(self.col.stdout.readline())
+
+    def check_parsing(self, process):
+        # Check parsing of values to read
+        c = re.compile("Address (\d+) => (\d+) values \((\w+)\)")
+
+        for d in self.expected_values:
+            m = c.match(process.stdout.readline())
             self.assertEqual(m.group(1), str(d['a']))
             self.assertEqual(m.group(2), str(d['l']))
             self.assertEqual(m.group(3), d['type'])
 
-        line = self.col.stdout.readline()
-        if self.is_master:
-            self.assertEqual(line, "Opening /dev/ttyUSB0 at 115200 bauds (N, 8, 1)\n")
-        else:
-            self.assertEqual(line, "Connecting to 127.0.0.1:1502\n")
-
+    def check_recorder(self, recorder, server_name):
         # MBRECORDER
         # Ignore remaining output for collector and check recorder
         c = re.compile("mb_%s_(\d+) ([\d\.]+)([\||\n]?)" % server_name)
 
-        for d in expected_values:
-            line = self.rec.stdout.readline()
+        for d in self.expected_values:
+            line = recorder.stdout.readline()
 
             if d['type'].find('int') == 0:
                 l = d['l']
@@ -95,16 +61,67 @@ class MasterTestCase(unittest.TestCase):
                 start = m.end()
 
 
-class ClientTestCase(MasterTestCase):
+class MasterTestCase(CommonMSTestCase):
 
     def setUp(self):
+        super(MasterTestCase, self).setUp()
+        if os.path.exists("/tmp/mbsocket"):
+            os.unlink("/tmp/mbsocket")
+
+        self.uts = Popen(["./unit-test-server", "rtu"], stdout=PIPE, stderr=PIPE)
+        self.rec = Popen(["../src/mbrecorder"], stdout=PIPE)
+        self.col = Popen(["../src/mbcollect", "-f", "master-test-case.ini"], stdout=PIPE)
+
+    def tearDown(self):
+        self.col.send_signal(signal.SIGTERM)
+        self.rec.send_signal(signal.SIGTERM)
+        self.uts.send_signal(signal.SIGTERM)
+        self.col.wait()
+        self.rec.wait()
+        self.uts.wait()
+
+    def test_output(self):
+        line = self.col.stdout.readline()
+        m = re.search("Slave name (\w+), ID (\d+)", line)
+        self.assertEqual(m.group(2), "1")
+
+        server_name = m.group(1)
+        self.assertEqual(server_name, "hello")
+
+        self.check_parsing(self.col)
+
+        line = self.col.stdout.readline()
+        self.assertEqual(line, "Opening /dev/ttyUSB0 at 115200 bauds (N, 8, 1)\n")
+
+        self.check_recorder(self.rec, server_name)
+
+
+class ClientTestCase(CommonMSTestCase):
+
+    def setUp(self):
+        super(ClientTestCase, self).setUp()
+
         if os.path.exists("/tmp/mbsocket"):
             os.unlink("/tmp/mbsocket")
 
         self.uts = Popen(["./unit-test-server", "tcp"], stdout=PIPE, stderr=PIPE)
         self.rec = Popen(["../src/mbrecorder"], stdout=PIPE)
         self.col = Popen(["../src/mbcollect", "-f", "client-test-case.ini"], stdout=PIPE)
-        self.is_master = False
+
+    def test_output(self):
+        line = self.col.stdout.readline()
+        m = re.search("Server name (\w+), IP ([\.\d]+):(\d+)", line)
+        self.assertEqual(m.group(2), "127.0.0.1")
+        self.assertEqual(m.group(3), "1502")
+
+        server_name = m.group(1)
+        self.assertEqual(server_name, "hello")
+
+        self.check_parsing(self.col)
+
+        line = self.col.stdout.readline()
+        self.assertEqual(line, "Connecting to 127.0.0.1:1502\n")
+        self.check_recorder(self.rec, server_name)
 
 
 class SlaveTestCase(unittest.TestCase):
@@ -120,9 +137,10 @@ class SlaveTestCase(unittest.TestCase):
     def tearDown(self):
         self.col.send_signal(signal.SIGTERM)
         self.rec.send_signal(signal.SIGTERM)
+        # Client stops himself
         self.col.wait()
         self.rec.wait()
-        # Client stops himself
+        self.uts.wait()
 
     def test_recorder(self):
         # Client write only one register
